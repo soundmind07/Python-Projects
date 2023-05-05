8b9940a67f66ff8344f80ec6133709051b40dff6
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2023-05-05 20:27:24 +0530

    Update qrcodegenerator.py

--- qrcode/qrcodegenerator.py
+++ qrcode/qrcodegenerator.py
@@ -1,3 +1,12 @@
+"""
+Project Learnings:-
+1- used qrcode library of python
+2- customised border and box size value to generate customised qr code
+3- for color properties i used fill color and back color
+4- on scanning this qr code u will be receive a link that takes u to my git hub profile.
+
+"""
+
 import qrcode 
 from PIL import Image 
 
