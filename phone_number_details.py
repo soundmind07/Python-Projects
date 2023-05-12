e871d0639dd8856875ab58c1f9555acdcd6a9033
Pallavit Saxena <121334016+soundmind07@users.noreply.github.com>
2023-05-12 13:51:24 +0530

    Update phone_number_details.py

--- phone_number_details.py
+++ phone_number_details.py
@@ -1,13 +1,18 @@
+"""
+Project Learnings:-
+1- phonenumbers is one of the modules that provides numerous features like providing basic information of a phone number, validation of a phone number etc.
+"""
+
 import phonenumbers
 from phonenumbers import timezone,geocoder,carrier
 
 number=input("enter your phonenumber with +___ ")
-phone=phonenumbers.parse(number)    # it is converting phone number entered by user into phone number format
+phone=phonenumbers.parse(number)                                 # it is converting phone number entered by user into phone number format
 
-if phonenumbers.is_valid_number(phone)==True: #checks whether the given phone number is valid or not
-     time=timezone.time_zones_for_number(phone)   # gives the timezone of our phone number
-     car=carrier.name_for_number(phone,"en")    #provides the carrier of our phone number
-     region=geocoder.description_for_number(phone,"en")   #gives the geographical location of our phone number
+if phonenumbers.is_valid_number(phone)==True:                    #checks whether the given phone number is valid or not
+     time=timezone.time_zones_for_number(phone)                  # gives the timezone of our phone number
+     car=carrier.name_for_number(phone,"en")                     #provides the carrier of our phone number
+     region=geocoder.description_for_number(phone,"en")          #gives the geographical location of our phone number
      print("Time Zone of your phone number is ",time )
      print("Carrier of your phone number is ",car)
      print("Your phone number is from ",region)
